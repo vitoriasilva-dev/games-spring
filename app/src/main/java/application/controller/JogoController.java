1c0ab86df85ed060025513d75169b700cd092316
Vitória Gabriela da Silva <vitoria.carneiro2512@gmail.com>
2025-03-10 17:55:49 +0000

    salva

---
+++ app/src/main/java/application/controller/JogoController.java
@@ -0,0 +1,9 @@
+package application.controller;
+
+import java.util.HashSet;
+import java.util.Optional;
+import java.util.Set;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
